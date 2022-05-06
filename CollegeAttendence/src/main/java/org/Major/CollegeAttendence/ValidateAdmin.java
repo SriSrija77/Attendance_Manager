c94bcb56fd7b0fce02db84f777f4a617a130306d
Viraj Panchal <98440474+virajpanchal7dev@users.noreply.github.com>
2022-05-05 23:45:51 -0500

    Added comments

--- CollegeAttendence/src/main/java/org/Major/CollegeAttendence/ValidateAdmin.java
+++ CollegeAttendence/src/main/java/org/Major/CollegeAttendence/ValidateAdmin.java
@@ -18,7 +18,10 @@ import org.Major.CollegeAttendence.DatabaseConnection;
 /**
  * Servlet implementation class ValidateAdmin
  */
+
+// Validate Admin Class 
 public class ValidateAdmin extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{ 
 		PrintWriter out=response.getWriter();
